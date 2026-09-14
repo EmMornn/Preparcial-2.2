667cd7ea3dbfbd3b39f92d64b52196c1781f9823
EmMornn <00000426>
2026-09-14 12:05:06 -0600

    Creacion de feature/funcion_registro

--- preparcial.cpp
+++ preparcial.cpp
@@ -11,3 +11,28 @@ struct Elemento
     float factor_utilizacion;
     bool estado_seguridad;
 };
+
+void registrarElementos(Elemento &elementos)
+{
+    string nombre;
+    cout << "Ingrese el codigo del elemento: ";
+    cin >> elementos.codigo;
+    cout << endl;
+    cout << "Ingrese el nombre del elemento: ";
+    cin >> elementos.nombre;
+    cout << endl;
+    cout << "Ingrese la longitud del elemento: ";
+    cin >> elementos.longitud;
+    cout << "Ingrese la Carga 1 del elemento: ";
+    cin >> elementos.cargas[0];
+    cout << endl;
+    cout << "Ingrese la Carga 2 del elemento: ";
+    cin >> elementos.cargas[1];
+    cout << endl;
+    cout << "Ingrese la Carga 3 del elemento: ";
+    cin >> elementos.cargas[2];
+    cout << endl;
+    cout << "Ingrese la capacidad maxima del elemento: ";
+    cin >> elementos.capmax;
+    cout << endl;
+};
