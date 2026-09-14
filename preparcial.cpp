cfefaa6bc43045e0f0bfee7d6edc47adb879ab51
EmMornn <00000426>
2026-09-14 12:01:39 -0600

    Creacion de feature/structure_elemento

--- preparcial.cpp
+++ preparcial.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+struct Elemento
+{
+    string codigo;
+    string nombre;
+    int longitud;
+    float cargas[3];
+    float capmax;
+    float factor_utilizacion;
+    bool estado_seguridad;
+};
