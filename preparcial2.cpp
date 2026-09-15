65489db87a806d7460484f29b225fb6331e101df
EmMornn <00000426>
2026-09-15 13:11:36 -0600

    feature/registro_estacion

--- preparcial2.cpp
+++ preparcial2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct estacion
+struct Estacion
 {
     string codigo;
     string ubicacion;
@@ -11,3 +11,21 @@ struct estacion
 };
 
 
+void registrar_Estacion(Estacion &estacion){
+cout << endl;
+cout << "Ingrese el codigo de la Estacion: ";
+cin >> estacion.codigo;
+cout << endl;
+cout << "Ingrese la ubicacion de la Estacion: ";
+getline(cin, estacion.ubicacion);
+cout << endl;
+cout << "Ingrese la lectura 1 de la Estacion: ";
+cin >> estacion.lecturas[0];
+cout << endl;
+cout << "Ingrese la lectura 2 de la Estacion: ";
+cin >> estacion.lecturas[1];
+cout << endl;
+cout << "Ingrese la lectura 3 de la Estacion: ";
+cin >> estacion.lecturas[2];
+cout << endl;
+};
