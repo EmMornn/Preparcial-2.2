b4b5fc439a24e209cdef5463421f755a397ae15e
EmMornn <00000426>
2026-09-15 12:44:37 -0600

    feature/estacion

--- preparcial2.cpp
+++ preparcial2.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+struct estacion
+{
+    string codigo;
+    string ubicacion;
+    float lecturas[3];
+    float promedio_intensidad;
+    string nivel_alerta;
+};
+
+
